842267c4f1009b480014b52219a8ae6da840ce2e
Rob Sawyer <robksawyer@gmail.com>
2021-12-13 22:33:55 +0900

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable eslint-comments/no-unlimited-disable */
+/* eslint-disable */
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'postcss-nested': {},
+    'postcss-custom-media': {},
+    tailwindcss: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  },
+}
